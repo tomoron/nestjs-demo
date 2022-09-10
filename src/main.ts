f7e66509dc70b2136a8cbfd942e862f9454a069c
tomoron <tomoron326@outlook.jp>
2022-09-10 20:41:34 +0900

    add:env.d.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+if (import.meta.env.PROD) {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  }
+  bootstrap();
 }
-bootstrap();
+
+export const ViteNodeApp = NestFactory.create(AppModule);
